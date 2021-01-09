a34226b141e43c10caba14bda36543cf648db67f
Iurii Bairamukov <juribai@mail.ru>
2021-01-09 21:21:12 +0300

    feat: реализовать адаптивность Header и Navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation';
 
 const Header = ({ loggedIn, location }) => {
-  console.log('location', location);
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
   return (
     <header
-      className={`header ${location === '/saved-news' ? 'header__white' : ''}`}
+      className={`header ${location === '/saved-news' ? 'header__white' : ''} ${
+        clicked ? 'header_dark' : ''
+      }`}
     >
-      <div className="header__top">
+      <div className="header__container">
         <Link
           to="/"
           className={`header__link header__link_main ${
-            location === '/saved-news' ? 'header__link_black' : ''
+            clicked
+              ? ''
+              : location === '/saved-news'
+              ? 'header__link_black'
+              : ''
           }`}
         >
           NewsExplorer
         </Link>
-        <Navigation loggedIn={loggedIn} location={location} />
+        <Navigation
+          loggedIn={loggedIn}
+          location={location}
+          onClick={handleClick}
+          clicked={clicked}
+        />
       </div>
     </header>
   );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,26 +3,50 @@ import './Navigation.css';
 import { ReactComponent as Logout } from '../../images/exit.svg';
 import { NavLink } from 'react-router-dom';
 
-const Navigation = ({ loggedIn, location }) => {
-  const blackClass =
-    location === '/saved-news'
-      ? {
-          link: 'navbar__link_black',
-          selectLink: 'navbar__link_select_black',
-          btn: 'navbar__btn_black',
-          btnLogout: 'navbar__btn-logout_black',
-        }
-      : '';
+const Navigation = ({ loggedIn, location, onClick, clicked }) => {
   loggedIn = true;
+
+  const handleClick = () => {
+    onClick();
+  };
+
+  const blackClass = {
+    link: 'navbar__link_black',
+    selectLink: 'navbar__link_select_black',
+    btn: 'navbar__btn_black',
+    btnLogout: 'navbar__btn-logout_black',
+    menuIcon: 'navbar__menu-icon-item_dark',
+  };
+
   return (
     <nav className="navbar">
-      <ul className="navbar__list">
+      <div className="navbar__menu-icon" onClick={handleClick}>
+        <span
+          className={`navbar__menu-icon-item ${
+            clicked
+              ? 'navbar__menu-icon-item_active'
+              : location === '/saved-news'
+              ? blackClass.menuIcon
+              : ''
+          } `}
+        ></span>
+      </div>
+
+      <ul
+        className={
+          clicked ? 'navbar__list navbar__list_active' : 'navbar__list'
+        }
+      >
         <li className="navbar__list-item">
           <NavLink
             exact
             to="/"
-            activeClassName={`navbar__link_select ${blackClass.selectLink}`}
-            className={`navbar__link ${blackClass.link}`}
+            activeClassName={`navbar__link_select ${
+              location === '/saved-news' ? blackClass.selectLink : ''
+            }`}
+            className={`navbar__link ${
+              clicked ? '' : location === '/saved-news' ? blackClass.link : ''
+            }`}
           >
             Главная
           </NavLink>
@@ -32,8 +56,16 @@ const Navigation = ({ loggedIn, location }) => {
             <NavLink
               exact
               to="/saved-news"
-              activeClassName={`navbar__link_select ${blackClass.selectLink}`}
-              className={`navbar__link ${blackClass.link}`}
+              activeClassName={`navbar__link_select ${
+                clicked
+                  ? ''
+                  : location === '/saved-news'
+                  ? blackClass.selectLink
+                  : ''
+              }`}
+              className={`navbar__link ${
+                clicked ? '' : location === '/saved-news' ? blackClass.link : ''
+              }`}
             >
               Сохраненные статьи
             </NavLink>
@@ -45,23 +77,46 @@ const Navigation = ({ loggedIn, location }) => {
           {loggedIn ? (
             <button
               arial-label="Выйти из аккаунта"
-              className={`navbar__link navbar__btn ${blackClass.link} ${blackClass.btn}`}
+              className={`navbar__link navbar__btn ${
+                clicked
+                  ? ''
+                  : location === '/saved-news'
+                  ? blackClass.link + ' ' + blackClass.btn
+                  : ''
+              }`}
             >
               UserName
               <Logout
-                className={`navbar__btn-logout ${blackClass.btnLogout}`}
+                className={`navbar__btn-logout ${
+                  clicked
+                    ? ''
+                    : location === '/saved-news'
+                    ? blackClass.btnLogout
+                    : ''
+                }`}
               />
             </button>
           ) : (
             <button
               arial-label="Залогиниться"
-              className={`navbar__link navbar__btn ${blackClass.link} ${blackClass.btn}`}
+              className={`navbar__link navbar__btn ${
+                clicked
+                  ? ''
+                  : location === '/saved-news'
+                  ? blackClass.link + ' ' + blackClass.btn
+                  : ''
+              }`}
             >
               Авторизоваться
             </button>
           )}
         </li>
       </ul>
+      <div
+        className={
+          clicked ? 'navbar__hover navbar__hover_active' : 'navbar__hover'
+        }
+      ></div>
     </nav>
   );
 };
