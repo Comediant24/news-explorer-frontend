68f7f209eeabf6c582dcf7a2c713a5e394d49f6d
Iurii Bairamukov <juribai@mail.ru>
2021-01-12 18:24:39 +0300

    feat: создать SavedNews и  SavedNewsHeader

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,12 @@ import { Route, Switch, useLocation } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import SavedNews from '../SavedNews/SavedNews';
 import './App.css';
 
 function App() {
   const { pathname } = useLocation();
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(true);
 
   return (
     <div className="app">
@@ -15,6 +16,9 @@ function App() {
       <Switch>
         <Route exact path="/">
           <Main location={pathname} />
+        </Route>
+        <Route exact path="/saved-news">
+          <SavedNews location={pathname} />
         </Route>
       </Switch>
       <Footer />
---
+++ src/components/SavedNews/SavedNews.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SavedNewsHeader from '../SavedNewsHeader/SavedNewsHeader';
+import './SavedNews.css';
+
+const SavedNews = () => {
+  return (
+    <section className="savednews">
+      <SavedNewsHeader />
+    </section>
+  );
+};
+
+export default SavedNews;
