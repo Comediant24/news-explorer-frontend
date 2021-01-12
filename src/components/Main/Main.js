2ea867e7ec00197e6e654ac0acaa206c216f315c
Iurii Bairamukov <juribai@mail.ru>
2021-01-12 18:33:45 +0300

    feat: создать компонент Preloader

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import About from '../About/About';
 import NewsCardList from '../NewsCardList/NewsCardList';
+import Preloader from '../Preloader/Preloader';
 import SearchForm from '../SearchForm/SearchForm';
 
 const Main = ({ location }) => {
   return (
     <main className="main">
       <SearchForm />
+      <Preloader />
       <NewsCardList location={location} />
       <About />
     </main>
